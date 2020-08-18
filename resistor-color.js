eb9537c0811711c2cbb598ca5287e3eeb717e3cc
rufio39 <69258446+rufio39@users.noreply.github.com>
2020-08-18 08:25:33 -0600

    added resitor-color

---
+++ resistor-color.js
@@ -0,0 +1,34 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (COLORS) => {
+
+switch (COLORS) {
+  case "black": return 0
+  break;
+  case "brown":return 1
+  break;
+  case "red":return 2
+  break;
+  case "orange":return 3
+  break;
+  case "yellow":return 4
+  break;
+  case "green":return 5
+  break;
+  case "blue":return 6
+  break;
+  case "violet":return 7
+  break;
+  case "grey":return 8
+  break;
+  case "white":return 9
+  break;
+
+}
+return COLORS
+};
+
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"] 
